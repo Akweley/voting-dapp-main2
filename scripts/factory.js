cf521eb57a38e5f2cfbf72fd828601bf2161d848
Akweley <akweleyokaiofficial@gmail.com>
2023-09-12 11:22:13 +0000

    new commit

--- scripts/factory.js
+++ scripts/factory.js
@@ -6,27 +6,27 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+    // Hardhat always runs the compile task when running scripts with its command
+    // line interface.
+    //
+    // If this script is run directly using `node` you may want to call compile
+    // manually to make sure everything is compiled
+    // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Factory = await hre.ethers.getContractFactory("Factory");
-  const factory = await Factory.deploy();
+    // We get the contract to deploy
+    const Factory = await hre.ethers.getContractFactory("Factory");
+    const factory = await Factory.deploy();
 
-  await factory.deployed();
+    await factory.deployed();
 
-  console.log("factory deployed to:", factory.address);
+    console.log("factory deployed to:", factory.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
